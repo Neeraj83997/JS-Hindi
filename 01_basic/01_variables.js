98218989963960ba17f658aebfef3aa5093e2c76
Neeraj83997 <neerajpatel83997@gmail.com>
2026-06-14 08:05:28 +0000

    variables and datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+
+const accountId=2550
+let emailId="Nrj@gmail.com"
+var accountPwd="Nrj@123"
+accountState="UP"
+let accountCity
+
+//accountId=12 // not allowed
+// prefered not to use var, because of issue in block scope and functional scope
+
+emailId="Neerak@12345"
+accountPwd="321"
+accountState="Punjab"
+
+console.log("Neeraj")
+console.table([accountId,emailId,accountPwd,accountState,accountCity])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node js not brouser
+
+let name="Neeraj" 
+let age= 21  
+let isLoggedIn= false 
+let temperature=null
+let city;
+// ** Primitive datatypes **
+// String=""
+// number-> 2 to power 53 // BigInt for big number
+// boolean -> true/false
+// null -> standalone value
+// undefined
+//symbol -> use for uniqueness
+
+// ** Non primitive datatypes **
+// Object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof temperature) // o/p -> Object
+console.log(typeof null) // o/p -> Object
+console.log(typeof city) // o/p -> undefined
+console.log(typeof undefined) // o/p -> undefined
